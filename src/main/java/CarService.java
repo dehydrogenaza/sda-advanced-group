71ff4d21321942689202be73be55897a980ba0d3
STJimmmy <tox85@mail.ru>
2022-12-08 19:14:14 +0100

    Methods to add cars to the list + getting the cheapest car (object);

--- src/main/java/CarService.java
+++ src/main/java/CarService.java
@@ -7,4 +7,19 @@ public class CarService {
     public CarService(List<Car> cars) {
         this.cars = cars;
     }
+
+    public void addCars(Car car) {
+        cars.add(car);
+    }
+
+    public Car getCheapestCar(List<Car> cars) {
+        Car cheapestCar = null;
+        int cheapestCarPrice = 0;
+        for (Car car : cars) {
+            if (car.getPrice() < cheapestCarPrice) {
+                cheapestCar = car;
+            }
+        }
+        return cheapestCar;
+    }
 }
